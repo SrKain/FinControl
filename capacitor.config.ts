b150a2709a01ea241a45a39355f57dbc82ad0ba0
Kauan Iasin <kauan@iasin.dev.br>
2026-01-09 15:41:58 -0300

    Versão Android em criação

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.FinControl.app',
+  appName: 'Fin Control',
+  webDir: 'dist'
+};
+
+export default config;
